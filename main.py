89f9c0fb582e5e6bad6dd86e2ef3d018ab49d32b
coachhallsomeinc <justin.hall@student.awesomeinc.org>
2023-11-06 01:35:01 +0000

    first commit

--- main.py
+++ main.py
@@ -1,8 +1,42 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
 
+fake_db = {}
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/items/")
+async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {"token": token}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
+@app.put("/items/{id}")
+def update_item(id: str, item: Item):
+    json_compatible_item_data = jsonable_encoder(item)
+    fake_db[id] = json_compatible_item_data
+    return fake_db
+
